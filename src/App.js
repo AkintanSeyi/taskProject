82136eea321d7e012d7ec0060faca2f768fd04d7
AkintanSeyi <seyiakintan@yahoo.com>
2024-03-11 12:57:03 -0700

    TaskProject

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import { useWeb3React } from '@web3-react/core';
+import { injected } from './components/wallet/Connectors';
 
 function App() {
+
+  const { activate, account, deactivate, active , chainId } = useWeb3React()
+
+  const connectMetamask = async () => {
+    try {
+      await activate(injected)
+    } catch (error) {
+
+    }
+  }
+  const disconnectMetamask = async () => {
+    try {
+      await deactivate()
+    } catch (error) {
+
+    }
+  }
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <br />
+      <br />
+      <br />
+
+      {active ? 'connected' : "not connected"}
+
+      <br />
+
+
+      {active ?
+        <button className="disconnectwallet" onClick={disconnectMetamask}>Disconnect Wallet</button>
+
+        :
+        <button className="connectwallet" onClick={connectMetamask}>Connect Wallet</button>
+
+
+      }
+
+      <br />
+      <br />
+      <br />
+      {active &&
+        <>
+        <span className='account'>{account}</span>
+        <br />
+        <br />
+        <span className='chainid'>{chainId}</span>
+
+        </>
+      }
+
+
     </div>
   );
 }
